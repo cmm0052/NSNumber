0f35cb392a9753dc1fa3942ade8f7ba77c498ab1
cmm0052 <cmm0052@auburn.edu>
2015-08-18 18:23:40 -0500

    Merge branch 'BeauBranch' into testing
    Conflicts:
    	NSNumber/AppDelegate.h

--- NSNumber/AppDelegate.h
+++ NSNumber/AppDelegate.h
@@ -5,7 +5,8 @@
 //  Created by Christopher Munna on 8/18/2015
 //  Copyright (c) 2012 Christopher Munna. All rights reserved.
 //
-// Beau's comments -- Github blows
+// GitHub really blows
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
